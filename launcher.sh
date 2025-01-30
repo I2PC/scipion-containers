83b93146b0efa834a979b805ae911e75fc0becfd
Ratolon <miceta@cnb.csic.es>
2025-01-30 14:46:20 +0100

    Update for CS compatibility

--- launcher.sh
+++ launcher.sh
@@ -16,18 +16,23 @@ CONTAINER_VERSION="latest"
 CONTAINER_FLAVOUR="spa"
 
 # You will need to fill in CryoAssess form in order to get your personal copy of the models
-CRYOASSESS_MODELS_DIR=/route/to/cryoasses_model_folder
+export CRYOASSESS_MODELS_DIR=/route/to/cryoasses_model_folder
+
 # CS will work only if the container has direct access to the cryosparcm binary
 # Point the container to the folder that contains the cryosparc_master folder
-CRYOSPARC_HOME_DIR=/route/to/cryosparc_master_containing_folder
-CRYOSPARC_PROJECTS_DIR=/route/to/cryosparc_scipion_projects_folder
 export CRYOSPARC_ACCOUNT="mycryosparc@email.com"
 export CRYOSPARC_PASSWORD="mypassword"
+export CRYOSPARC_HOME="/route/to/cryosparc_master_containing_folder"
+export CRYO_PROJECTS_DIR="/route/to/cryosparc_scipion_projects_folder"
+
+# SCIPION project folder configuration
+export SCIPION_USER_DATA="/path/to/scipion_project_storage"
+export SCIPION_LOGS="$SCIPION_USER_DATA/logs"
+export SCIPION_LOG="$SCIPION_LOGS/scipion.log"
+export SCIPION_TESTS_OUTPUT="$SCIPION_USER_DATA/Tests"
 
 # The datadir will be used to input the RAW data used for processing, ie movies/tiltseries
-SCIPION_DATADIR=/route/to/your_raw_data_dir
-# The projdir will house Scipion's project and all of its intermediate data
-SCIPION_PROJDIR=/route/to/your_big_storage_for_projects
+RAW_DATADIR=/route/to/your_raw_data_dir
 
 ###
 ###
@@ -40,8 +45,11 @@ echo "Pulling version $CONTAINER_VERSION from branch $CONTAINER_FLAVOUR"
 apptainer pull oras://rinchen.cnb.csic.es/apptainer/scipion-$CONTAINER_FLAVOUR:$CONTAINER_VERSION
 
 echo "Launching Scipion container for $CONTAINER_FLAVOUR"
-apptainer exec --nv scipion-$CONTAINER_FLAVOUR:$CONTAINER_VERSION \
---bind $CRYOSPARC_HOME_DIR:/cryosparc \
---bind $CRYOASSES_MODELS_DIR:/scipion/software/em/cryoassess_models \
---bind $SCIPION_DATADIR:/data \
---bind $SCIPION_PROJDIR:/scipion/ScipionUserData /scipion/scipion3
+apptainer exec --nv \
+--bind $CRYOSPARC_HOME \
+--bind $CRYO_PROJECTS_DIR \
+--bind $SCIPION_USER_DATA \
+--bind $RAW_DATADIR \
+#--bind $CRYOASSES_MODELS_DIR \
+scipion-${CONTAINER_FLAVOUR}_${CONTAINER_VERSION}.sif \
+/scipion/scipion3
