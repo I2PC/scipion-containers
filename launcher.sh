f6db3561ecfaedbf9f0b30d6d74def89f03a7ee8
Irene <irene.sanchezl93@gmail.com>
2025-10-13 16:12:20 +0200

    CHIMERA_CMD included in LAUNCH_CMD

--- launcher.sh
+++ launcher.sh
@@ -36,7 +36,7 @@ CHIMERA_HOME="path/to/your/local/chimerax-X.X.X"
 export CHIMERA_LOCATION="/scipion/software/em/chimerax-X.X.X"
 
 # UNCOMMENT THIS LINE WHEN USING CHIMERAX
-# CHIMERA_CMD=" --bind $CHIMERA_HOME:$CHIMERA_LOCATION --env CHIMERA_HOME=$CHIMERA_LOCATION"
+# _CMD=" --bind $_HOME:$CHIMERA_LOCATION --env CHIMERA_HOME=$CHIMERA_LOCATION"
 ### CHIMERAX END
 ### END #######################################################################
 
@@ -148,7 +148,7 @@ LAUNCH_CMD="apptainer exec --nv --containall \
             --env SCIPION_USER_DATA=$SCIPION_PROJDIR \
             --bind /run --bind /etc/resolv.conf \
             --bind $SCIPION_DATADIR:/data --bind $SCIPION_PROJDIR \
-            $SCIPCRYOSPARC_CMD $SCIPCRYOASSESS_CMD $SCIPPHENIX_CMD $SCIPSLURM_CMD $SCIPMPI_CMD "
+            $SCIPCRYOSPARC_CMD $SCIPCRYOASSESS_CMD $SCIPPHENIX_CMD $SCIPSLURM_CMD $SCIPMPI_CMD $CHIMERA_CMD "
 
 CONTAINER="$CONTAINER_LOCATION/$CONTAINER.sif"
 
